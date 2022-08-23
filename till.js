4a050c0a5ff47fa63c86944981419aa780bb511c
delphine <delphine.mubiligi@gmail.com>
2022-08-24 00:19:41 +0100

    test driving adding items to the list

--- till.js
+++ till.js
@@ -1,12 +1,20 @@
-const hipsterCoffee = require('./hipstercoffee.json');
+const fs = require('fs')
 
 class Till {
   constructor () {
-    this.items = {};
+    this.items = {}
+    this.shopDetails = JSON.parse(fs.readFileSync('./hipstercoffee.json'))[0];
+    this.menu = this.shopDetails.prices[0]; 
   }
 
   getItems () {
     return this.items;
+  }
+
+  addItem (item) {
+    if (item in this.menu && !(item in this.items)) {
+      this.items[item] = [this.menu[item]]
+    }
   }
 }
 
--- till.test.js
+++ till.test.js
@@ -6,4 +6,17 @@ describe('Till', () => {
     expect(till.getItems()).toEqual({})
   })
 
+  it('adds 1 item to this.items', () => {
+    const till = new Till;
+    till.addItem('Cafe Latte')
+    expect(till.getItems()).toEqual({"Cafe Latte": [4.75]})
+  });
+
+  it('adds multiple items to this.items', () => {
+    const till = new Till;
+    till.addItem('Cafe Latte')
+    till.addItem('Tea')
+    expect(till.getItems()).toEqual({"Cafe Latte": [4.75], "Tea": [3.65]})
+  });
+
 });
