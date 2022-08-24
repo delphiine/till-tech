f439528970862f467de069a2d80e50a5c0b658dc
delphine <delphine.mubiligi@gmail.com>
2022-08-24 01:29:19 +0100

    test driving adding the same item multiple times

--- till.js
+++ till.js
@@ -13,7 +13,10 @@ class Till {
 
   addItem (item) {
     if (item in this.menu && !(item in this.items)) {
-      this.items[item] = [this.menu[item]]
+      this.items[item] = [this.menu[item], 1]
+    } else if (item in this.items) {
+      this.items[item][0] += this.menu[item]
+      this.items[item][1] ++
     }
   }
 }
--- till.test.js
+++ till.test.js
@@ -9,14 +9,23 @@ describe('Till', () => {
   it('adds 1 item to this.items', () => {
     const till = new Till;
     till.addItem('Cafe Latte')
-    expect(till.getItems()).toEqual({"Cafe Latte": [4.75]})
+    expect(till.getItems()).toEqual({"Cafe Latte": [4.75, 1]})
   });
 
   it('adds multiple items to this.items', () => {
     const till = new Till;
     till.addItem('Cafe Latte')
     till.addItem('Tea')
-    expect(till.getItems()).toEqual({"Cafe Latte": [4.75], "Tea": [3.65]})
+    expect(till.getItems()).toEqual({"Cafe Latte": [4.75, 1], "Tea": [3.65, 1]})
+  });
+
+  it('adds multiple items to this.items', () => {
+    const till = new Till;
+    till.addItem('Tea')
+    expect(till.getItems()).toEqual({"Tea": [3.65, 1]})
+
+    till.addItem('Tea')
+    expect(till.getItems()).toEqual({"Tea": [7.3, 2]})
   });
 
 });
